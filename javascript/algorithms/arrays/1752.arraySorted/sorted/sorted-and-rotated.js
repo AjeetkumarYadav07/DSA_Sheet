4df1b8f44a3794cbbe2f98b2ba84f42df3b59dd4
AjeetkumarYadav07 <iamsupertrader07@gmail.com>
2025-08-29 23:50:04 +0530

    add new problem

---
+++ javascript/algorithms/arrays/1752.arraySorted/sorted/sorted-and-rotated.js
@@ -0,0 +1,10 @@
+// Problem link : https://leetcode.com/problems/check-if-array-is-sorted-and-rotated/description/ 
+
+function isSorted () {
+
+}
+
+const arr = [3,4,5,1,2] ;
+const arr2 = [2,1,3,4] ;
+const n = arr.length ;
+const result = isSorted(arr , n);
