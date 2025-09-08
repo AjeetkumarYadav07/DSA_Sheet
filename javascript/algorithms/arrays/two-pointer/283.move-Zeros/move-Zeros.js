85df48ab41e2004a5833e04abaabbaa2eea2d19b
AjeetkumarYadav07 <iamsupertrader07@gmail.com>
2025-09-08 23:46:36 +0530

    add moveZero leetcode problem

--- javascript/algorithms/arrays/two-pointer/283.move-Zeros/move-Zeros.js
+++ javascript/algorithms/arrays/two-pointer/283.move-Zeros/move-Zeros.js
@@ -22,5 +22,25 @@ function moveZero(arr){
    return result ;
 }
 
+
+/// Optimal Approach 
+
+function moveZero(arr){
+     let nonzero = 0 ;  // we can denote  as pointer1 (becouse using two pointer approach)
+
+     for(let i = 0 ; i<arr.length ; i++){
+        if(arr[i] != 0){
+            arr[nonzero] = arr[i]  // overwrite the zero's 
+            nonzero++ ;
+        }
+     }
+
+     for(let i = nonzero ; i<arr.length ; i++){
+        arr[i] = 0 ;
+     }
+
+     return arr ;
+}
+
 const result = moveZero([0,1,0,3,12]);
 console.log(result);
